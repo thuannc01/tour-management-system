3d104f24324bde6287da5ea1784118b1d3494c7d
Thuan Nguyen <80739892+thuannc01@users.noreply.github.com>
2023-10-01 13:21:26 +0700

    update

--- 02_Frontend/src/components/thuanInput/index.vue
+++ 02_Frontend/src/components/thuanInput/index.vue
@@ -1,32 +1,64 @@
 <script>
-import { mapState } from 'vuex';
-
 import template from './template.html';
 
 var ThuanInput = {
     template: template,
     props: {
         id: String,
-        label: String,
-        style: String,
-        itemClass: String,
-        labelClass: String,
+        type: String,
+        inputClass: String,
         placeholder: String,
-        value: String,
         isRequired: Boolean,
         isDisabled: Boolean,
         isReadonly: Boolean,
-        type: String,
-        maxLength: Number,
+        style: String,
         onChange: Function,
         onFocus: Function,
-        onBlur: Function
+        onBlur: Function,
+        onClick: Function,
+        label: String,
+        modelValue: String,
+        maxLength: Number
     },
+    emits: ['update:modelValue'],
     data() {
         return {};
     },
+    mounted() {},
     computed: {
-        ...mapState('app', [''])
+        Value: {
+            get() {
+                return this.modelValue;
+            },
+            set() {}
+        }
+    },
+    methods: {
+        focusInput() {
+            const val = this.$refs.input.value;
+            let value = val;
+            this.$emit('update:modelValue', value);
+        },
+        blurInput() {
+            const val = this.$refs.input.value;
+            let value = val;
+            this.$emit('update:modelValue', value);
+        },
+        inputKeydown() {
+            const val = this.$refs.input.value;
+            let value = val;
+            this.$emit('update:modelValue', value);
+        },
+        inputKeyup() {
+            const val = this.$refs.input.value;
+            let value = val;
+            this.$emit('update:modelValue', value);
+        },
+        updateInput() {
+            const val = this.$refs.input.value;
+            let value = val;
+            this.$emit('update:modelValue', value);
+        }
     }
 };
 export default ThuanInput;
--- 02_Frontend/src/components/thuanTextarea/index.vue
+++ 02_Frontend/src/components/thuanTextarea/index.vue
@@ -1,12 +1,63 @@
 <script>
-import { mapState } from 'vuex';
-
 import template from './template.html';
 
 var ThuanTextarea = {
     template: template,
+    props: {
+        id: String,
+        inputClass: String,
+        placeholder: String,
+        isRequired: Boolean,
+        isDisabled: Boolean,
+        isReadonly: Boolean,
+        customStyle: String,
+        onChange: Function,
+        onFocus: Function,
+        onBlur: Function,
+        onClick: Function,
+        label: String,
+        modelValue: String,
+        maxLength: Number
+    },
+    emits: ['update:modelValue'],
+    data() {
+        return {};
+    },
+    mounted() {},
     computed: {
-        ...mapState('app', [''])
+        Value: {
+            get() {
+                return this.modelValue;
+            },
+            set() {}
+        }
+    },
+    methods: {
+        focusInput() {
+            const val = this.$refs.input.value;
+            let value = val;
+            this.$emit('update:modelValue', value);
+        },
+        blurInput() {
+            const val = this.$refs.input.value;
+            let value = val;
+            this.$emit('update:modelValue', value);
+        },
+        inputKeydown() {
+            const val = this.$refs.input.value;
+            let value = val;
+            this.$emit('update:modelValue', value);
+        },
+        inputKeyup() {
+            const val = this.$refs.input.value;
+            let value = val;
+            this.$emit('update:modelValue', value);
+        },
+        updateInput() {
+            const val = this.$refs.input.value;
+            let value = val;
+            this.$emit('update:modelValue', value);
+        }
     }
 };
 export default ThuanTextarea;
