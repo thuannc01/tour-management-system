3e3a1bfc77f5af373baa518524e6ae319e79ef6c
Thuan Nguyen <80739892+thuannc01@users.noreply.github.com>
2023-10-04 23:59:51 +0700

    update fe

--- 02_Frontend/src/views/user/home/index.vue
+++ 02_Frontend/src/views/user/home/index.vue
@@ -3,13 +3,21 @@ import { mapActions, mapMutations, mapState } from 'vuex';
 import store from '@/store';
 import HomeStore from '@/views/user/home/store';
 import label from './label';
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+// Import Swiper styles
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
 import template from './template.html';
 import './style.scss';
 
 var Home = {
     template: template,
-    components: {},
+    components: {
+        Swiper,
+        SwiperSlide,
+        modules: [Navigation, Pagination, Scrollbar, A11y]
+    },
     beforeCreate() {
         if (!store.hasModule('HomeStore')) {
             store.registerModule('HomeStore', HomeStore);
@@ -33,7 +41,13 @@ var Home = {
         ...mapActions('app', []),
         ...mapMutations('app', ['showHeaderError', 'showModalMessage']),
         // module
-        ...mapActions('', [''])
+        ...mapActions('', ['']),
+        onSwiper(swiper) {
+            console.log(swiper);
+        },
+        onSlideChange() {
+            console.log('slide change');
+        }
     }
 };
 export default Home;
